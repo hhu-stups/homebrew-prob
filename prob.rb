baf0054c5879617e1a97e42741e2277a49c7ec68
David Schneider <david.schneider@bivab.de>
2019-07-12 12:43:06 +0200

    Updated formula to ProB 1.9.0

--- prob.rb
+++ prob.rb
@@ -2,9 +2,9 @@ require_relative 'lib/base-prob'
 
 class Prob < BaseProB
 
-  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.8.2/ProB.mac_os.x86_64.tar.gz"
-  version "1.8.2"
-  sha256 "5a93c02c8004ef5f38e219173524bd4da8a58dddd49493c3588b40d406ff2ca3"
+  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.9.0/ProB.mac_os.x86_64.tar.gz"
+  version "1.9.0"
+  sha256 "139a99ee748f6dc0acdd4297a794675298b2d3ae308cd67532c06c1d42390524"
 
   bottle :unneeded
 
