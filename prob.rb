8240c1a6b8c8c252f914caffca68125fbf09094a
Michael Leuschel <leuschel@uni-duesseldorf.de>
2020-03-06 14:49:39 +0100

    set version to 1.9.3

--- prob.rb
+++ prob.rb
@@ -2,7 +2,7 @@ require_relative 'lib/base-prob'
 
 class Prob < BaseProB
 
-  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.9.0/ProB.mac_os.x86_64.tar.gz"
+  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.9.3/ProB.mac_os.x86_64.tar.gz"
   version "1.9.0"
   sha256 "525af6b977bd09ff84909898d3dabc34bd9d19598defbcaa5270a21dec94504e"
 
