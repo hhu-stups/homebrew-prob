a251e2a1d34002f7cf726f19872988a725347fb2
Michael Leuschel <leuschel@uni-duesseldorf.de>
2021-12-30 10:54:37 +0100

    update formula for release 1.11.1
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- prob.rb
+++ prob.rb
@@ -2,9 +2,9 @@ require_relative 'lib/base-prob'
 
 class Prob < BaseProB
 
-  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.10.2/ProB_mac_os.x86_64.notarized.zip"
-  version "1.10.2"
-  sha256 "4041e78abcc392b2583f2d674520ac14ecf8513aadd4a696c71eb3b8b9887508"
+  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.11.1/ProB_mac_os.x86_64.notarized.zip"
+  version "1.11.1"
+  sha256 "22180c48682cabaa6714f6ee4055e1892d14ba9a69e02482f13bc3256a6bfe25"
 
   bottle :unneeded
 
