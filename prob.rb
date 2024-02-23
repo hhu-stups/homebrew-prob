1d7a7e4794395269669ccae6172b15b6eef67029
Michael Leuschel <leuschel@uni-duesseldorf.de>
2024-02-23 12:47:28 +0100

    update URLs
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- prob.rb
+++ prob.rb
@@ -2,7 +2,7 @@ require_relative 'lib/base-prob'
 
 class Prob < BaseProB
 
-  url "https://www3.hhu.de/stups/downloads/prob/tcltk/releases/1.13.0/ProB.macos.zip"
+  url "https://stups.hhu-hosting.de/downloads/prob/tcltk/releases/1.13.0/ProB.macos.zip"
   version "1.13.0"
   sha256 "1089c64d5a7365196a7c99eb037cbdc773c90c1d9ea8e7e2df171289701ea6c6"
 
@@ -12,6 +12,6 @@ class Prob < BaseProB
     # We use the current date to identify each nightly build
     version Time.now.strftime("%Y%m%d") + "-nightly"
 
-    url "https://www3.hhu.de/stups/downloads/prob/tcltk/nightly/ProB.mac_os.zip"
+    url "https://stups.hhu-hosting.de/downloads/prob/tcltk/nightly/ProB.mac_os.zip"
   end
 end
