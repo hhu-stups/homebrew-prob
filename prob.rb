4bbf91734d137dd6c93fe0f28f53ece56a5c031b
Michael Leuschel <leuschel@uni-duesseldorf.de>
2025-06-27 13:51:19 +0200

    update version to 1.15.0
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- prob.rb
+++ prob.rb
@@ -2,9 +2,9 @@ require_relative 'lib/base-prob'
 
 class Prob < BaseProB
 
-  url "https://stups.hhu-hosting.de/downloads/prob/tcltk/releases/1.13.0/ProB.macos.zip"
-  version "1.13.0"
-  sha256 "1089c64d5a7365196a7c99eb037cbdc773c90c1d9ea8e7e2df171289701ea6c6"
+  url "https://stups.hhu-hosting.de/downloads/prob/tcltk/releases/1.15.0/ProB.macos.zip"
+  version "1.15.0"
+  sha256 "f67ae309ab91bf169e40ad9c231d32ce902fd7729a3eaa0786d0e5467a70a5fa"
 
   bottle :unneeded
 
