3088c622fda856e60cedd4da1606cdd4e0a65a53
Michael Leuschel <leuschel@uni-duesseldorf.de>
2026-02-12 13:42:36 +0100

    remove bottle unneeded
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- prob.rb
+++ prob.rb
@@ -6,8 +6,6 @@ class Prob < BaseProB
   version "1.15.1"
   sha256 "6e85c88eee56ec7f18f4b1737693b1dc4bfbf4f9081c42def8c09afff48f24c2"
 
-  bottle :unneeded
-
   head do
     # We use the current date to identify each nightly build
     version Time.now.strftime("%Y%m%d") + "-nightly"
