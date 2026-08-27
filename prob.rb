b288ea0c51ea9d598511fe6484ac04f69973dd65
Michael Leuschel <leuschel@uni-duesseldorf.de>
2026-08-27 16:46:45 +0200

    fix version number
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- prob.rb
+++ prob.rb
@@ -3,7 +3,7 @@ require_relative 'lib/base-prob'
 class Prob < BaseProB
 
   url "https://stups.hhu-hosting.de/downloads/prob/tcltk/releases/1.16.1/ProB.macos.zip"
-  version "1.16.0"
+  version "1.16.1"
   sha256 "93cb8e8581ac15b327800d423c6588b134b589e87ea34abc51d6d00a7b8cedc3"
 
   head do
